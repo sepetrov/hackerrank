ccf5fb02ea197a2c608b3932b5935ae90e077e22
Stanislav Petrov <s.e.petrov@gmail.com>
2019-12-17 17:20:38 +0200

    Fix calculation of number of layers

--- matrix_rotation/matrix_rotation.go
+++ matrix_rotation/matrix_rotation.go
@@ -12,69 +12,59 @@ func MatrixRotation(matrix [][]int32, r int32) {
 	rows := len(matrix)
 	cols := len(matrix[0])
 
-	l := int(math.Round(math.Max(float64(rows), float64(cols))) / 2.0)
-	layers := make([][]int32, l)
+	l := int(math.Round(math.Min(float64(rows), float64(cols))) / 2.0)
 
-	// Walk the matrix clockwise starting from top left and collect the values
-	// for each layer.
 	for i := 0; i < l; i++ {
 		layer := []int32{}
+
+		// Walk the matrix clockwise starting from top left and collect the values
+		// for each layer.
 
 		// top left to top right
 		for j := i; j < cols-i-1; j++ {
 			layer = append(layer, matrix[i][j])
 		}
-
 		// top right to bottom right
 		for j := i; j < rows-i-1; j++ {
 			layer = append(layer, matrix[j][cols-i-1])
 		}
-
 		// bottom right to bottom left
 		for j := cols - i - 1; j > i; j-- {
 			layer = append(layer, matrix[rows-i-1][j])
 		}
-
 		// bottom left to top left
 		for j := rows - i - 1; j > i; j-- {
 			layer = append(layer, matrix[j][i])
 		}
 
-		layers[i] = layer
-	}
-
-	// Left shift each layer r times.
-	for i := 0; i < len(layers); i++ {
+		// Left shift layer r times.
 		for j := int32(0); j < r; j++ {
-			layers[i] = append(layers[i][1:], layers[i][0])
+			layer = append(layer[1:], layer[0])
 		}
-	}
 
-	// Populate the matrix with the values from the layers.
-	for i := 0; i < l; i++ {
+		// Populate the matrix with the values from the layer.
+
+		c := 0
 
 		// top left to top right
 		for j := i; j < cols-i-1; j++ {
-			matrix[i][j] = layers[i][0]
-			layers[i] = layers[i][1:]
+			matrix[i][j] = layer[c]
+			c++
 		}
-
 		// top right to bottom right
 		for j := i; j < rows-i-1; j++ {
-			matrix[j][cols-i-1] = layers[i][0]
-			layers[i] = layers[i][1:]
+			matrix[j][cols-i-1] = layer[c]
+			c++
 		}
-
 		// bottom right to bottom left
 		for j := cols - i - 1; j > i; j-- {
-			matrix[rows-i-1][j] = layers[i][0]
-			layers[i] = layers[i][1:]
+			matrix[rows-i-1][j] = layer[c]
+			c++
 		}
-
 		// bottom left to top left
 		for j := rows - i - 1; j > i; j-- {
-			matrix[j][i] = layers[i][0]
-			layers[i] = layers[i][1:]
+			matrix[j][i] = layer[c]
+			c++
 		}
 	}
 
